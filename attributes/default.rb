01ac390faa8b0752b02f72d48067a4d2ca8c68b3
stephaneLII <stephane.lii@informatique.gov.pf>
2015-02-25 09:27:58 -1000

    Ajustement des attributs par défaut.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['chef-postgis']['databases'] = [ 'db1' ]
+default['chef-postgis']['databases'] = []
 default['chef-postgis']['src_link'] = 'http://download.osgeo.org/postgis/source/postgis-2.1.3.tar.gz'
 default['chef-postgis']['directory']['src'] = '/opt/postgis/src'
 default['chef-postgis']['bin'] = 'postgis-2.1.3.tar.gz'
